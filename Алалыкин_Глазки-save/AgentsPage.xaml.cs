c8c1e5dcafbadd2f95810be748551ea549db26cb
rishtim <russskiy.hyusten@gmail.com>
2026-02-13 15:10:02 +0500

    лаб 17

--- Алалыкин_Глазки-save/AgentsPage.xaml.cs
+++ Алалыкин_Глазки-save/AgentsPage.xaml.cs
@@ -23,11 +23,59 @@ namespace Алалыкин_Глазки_save
         public AgentsPage()
         {
             InitializeComponent();
+            var currentAgent = AlalykinEyesEntities1.GetContext().Agent.ToList();
+            AgentListView.ItemsSource = currentAgent;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SortType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void UpdateAgents()
+        {
+            var currentServices = AlalykinEyesEntities1.GetContext().Service.ToList();
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.DiscountIt) >= 0 && Convert.ToInt32(p.DiscountIt) <= 100)).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.DiscountIt) >= 0 && Convert.ToInt32(p.DiscountIt) < 5)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.DiscountIt) >= 5 && Convert.ToInt32(p.DiscountIt) < 15)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.DiscountIt) >= 15 && Convert.ToInt32(p.DiscountIt) < 30)).ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.DiscountIt) >= 30 && Convert.ToInt32(p.DiscountIt) < 70)).ToList();
+            }
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentServices = currentServices.Where(p => (Convert.ToInt32(p.DiscountIt) >= 70 && Convert.ToInt32(p.DiscountIt) <= 100)).ToList();
+            }
+            currentServices = currentServices.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            AgentListView.ItemsSource = currentAgents.ToList();
+            AgentListView.ItemsSource = currentAgents;
         }
     }
 }
